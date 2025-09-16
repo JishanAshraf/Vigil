1e2e6aac506feda6f17efb95249c6611ecd7c070
Jishan Ashraf <jishanheena786@gmail.com>
2025-09-16 04:13:07 +0000

    revert

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,11 @@
 
 import { LoginForm } from '@/components/login-form';
-import Image from 'next/image';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-8 py-12">
       <div className="mx-auto grid w-full max-w-sm gap-6">
-        <Image
-          src="https://picsum.photos/seed/loginpage/600/400"
-          alt="Community photo"
-          width={600}
-          height={400}
-          className="rounded-lg shadow-lg"
-          data-ai-hint="community city"
-        />
-
         <Card>
           <CardHeader>
             <div className="grid gap-2 text-center">
