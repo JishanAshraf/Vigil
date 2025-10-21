c9f536bab99e9469cff7bf7d08bb171c99f8c597
Jishan Ashraf <jishanheena786@gmail.com>
2025-10-21 05:51:23 +0000

    go to the health page and put the AI on the bottom of the page

--- src/app/health/page.tsx
+++ src/app/health/page.tsx
@@ -7,7 +7,7 @@ export default function HealthPage() {
         <>
             <MainHeader />
             <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 md:pl-64 pb-28 md:pb-8">
-                <div className="flex flex-1 items-end justify-center w-full pb-8">
+                <div className="flex flex-1 items-end justify-center w-full">
                     <SymptomChecker />
                 </div>
             </main>
